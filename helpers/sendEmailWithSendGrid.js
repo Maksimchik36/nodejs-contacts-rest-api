475ed2f82cfd5013461747c8f87f689fdcb1e2f3
Maksimchik36 <Peltek1985@gmail.com>
2022-10-22 14:16:33 +0300

    add
    SEBDER to env

--- helpers/sendEmailWithSendGrid.js
+++ helpers/sendEmailWithSendGrid.js
@@ -2,12 +2,12 @@
 
 const sgMail = require('@sendgrid/mail')
 require("dotenv").config();
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, SENDER } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 
 const sendEmailWithSendGrid = async (data) => {
-  const mail = { ...data, from: 'Peltek1985@gmail.com' };
+  const mail = { ...data, from: SENDER };
   await sgMail.send(mail);
   return true;
 }
